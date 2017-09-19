6d73530fd84ce309db5b8006f0fc0bb87014a7f0
Kev Comedia <kevcomedia@gmail.com>
2017-09-19 16:00:39 +0800

    Render left month axis
    Also used d3.scaleTime for this axis. This axis spans an entire year,
    but the year itself is not relevant, hence the `null` values.
    
    It's important that the days are specified in the `new Date` call so the
    tick for December doesn't line up with the year axis.

--- src/d3.exports.js
+++ src/d3.exports.js
@@ -5,7 +5,7 @@
  * Export only from the modules that you need to keep the bundle size minimal.
  */
 export {extent, max, min} from 'd3-array';
-export {axisBottom} from 'd3-axis';
+export {axisBottom, axisLeft} from 'd3-axis';
 export {json} from 'd3-request';
 export {scaleLinear, scaleTime} from 'd3-scale';
 export {select, selectAll} from 'd3-selection';
--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ const width = 1100;
 const height = 500;
 const padding = {
   bottom: 50,
-  left: 50,
-  right: 50
+  left: 60,
+  right: 50,
+  top: 50
 };
 
 const svg = d3.select('#chart')
@@ -21,8 +22,17 @@ const svg = d3.select('#chart')
 const xScale = d3.scaleTime()
   .range([padding.left, width - padding.right]);
 
+const yScale = d3.scaleTime()
+  .domain([new Date(null, 0, 1), new Date(null, 11, 31)])
+  .range([padding.top, height - padding.bottom]);
+
 const yearAxis = svg.append('g')
   .attr('transform', `translate(0, ${height - padding.bottom})`);
+
+// Month axis. No variables needed because everything's set up here.
+svg.append('g')
+  .attr('transform', `translate(${padding.left}, 0)`)
+  .call(d3.axisLeft(yScale).ticks(12, '%B'));
 
 d3.json(temperatureDataUrl, ({baseTemperature, monthlyVariance}) => {
   // I passed a dummy second argument to `new Date()` because it interprets a
